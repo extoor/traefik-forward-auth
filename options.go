86177137be57ad8e52c38faf4a204ec1ab7a08e6
DevOps <avp.ukr@gmail.com>
2018-10-11 12:48:30 +0300

    Fix build

---
+++ options.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/base64"
+)
+
+func addPadding(secret string) string {
+	padding := len(secret) % 4
+	switch padding {
+	case 1:
+		return secret + "==="
+	case 2:
+		return secret + "=="
+	case 3:
+		return secret + "="
+	default:
+		return secret
+	}
+}
+
+// secretBytes attempts to base64 decode the secret, if that fails it treats the secret as binary
+func secretBytes(secret string) []byte {
+	b, err := base64.URLEncoding.DecodeString(addPadding(secret))
+	if err == nil {
+		return []byte(addPadding(string(b)))
+	}
+	return []byte(secret)
+}
